df01cbd899e5de2fc90399eb2d2c6e9c37d3f530
Sophie Virtue <sophie.virtue@gmail.com>
2018-12-04 16:52:16 -0800

    Edit assets folder, start prizes page

---
+++ themes/ume/page-prizes.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * @package UME
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area prizes">
+		<main id="main" class="site-main" role="main">
+
+		<img class="grid-background" src="<?php echo get_template_directory_uri(); ?>/assets/images/pink-neon-words/prizes.png" />	
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
