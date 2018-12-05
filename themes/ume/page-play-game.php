35bd3f5088bbf904e6f5e6bc02cc59a91236bcb4
Jeff Liknes <jeff.liknes@gmail.com>
2018-12-05 14:54:21 -0800

    new file playgame.php and play-game.scss

---
+++ themes/ume/page-play-game.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * @package UME
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area play-game">
+		<main id="main" class="site-main" role="main">
+			
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+			<?php endwhile; // End of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+
+<?php get_footer(); ?>
