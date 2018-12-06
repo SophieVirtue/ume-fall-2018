ac55dec6a7a16ae809f54e8d946eb5019a146ed9
Sophie Virtue <sophie.virtue@gmail.com>
2018-12-05 16:28:26 -0800

    Make prize page and header/footer responsive

--- themes/ume/page-prizes.php
+++ themes/ume/page-prizes.php
@@ -10,7 +10,7 @@ get_header(); ?>
 	<div id="primary" class="content-area prizes">
 		<main id="main" class="site-main" role="main">
 
-		<img class="page-name" src="<?php echo get_template_directory_uri(); ?>/assets/images/pink-neon-words/prizes.png" />	
+		<img class="prize-page-name" src="<?php echo get_template_directory_uri(); ?>/assets/images/pink-neon-words/prizes.png" />	
         <h3>Earn more coins to win these prizes for your game!</h3>
         <div class="coin-legend">
             <div class="coin-winnings">
