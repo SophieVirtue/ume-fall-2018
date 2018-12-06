e8ab1a0e65dbe3e7f0c6686e1a3e0cef32fd46af
Jenna Pegg <jenna.pegg.vic@gmail.com>
2018-12-05 16:28:30 -0800

    add numbers and background to leaderboard foreach loop

--- themes/ume/page-leaderboard.php
+++ themes/ume/page-leaderboard.php
@@ -10,32 +10,46 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
         <div class="leaderboard">
-			<img class="leader-title" src="<?php echo get_template_directory_uri() . '/assets/images/pink-neon-words/leaderboard.png' ?>" alt="">
+            <div class="leader-title">
+                <img src="<?php echo get_template_directory_uri() . '/assets/images/pink-neon-words/leaderboard.png' ?>" alt="">
+            </div>
+
             <?php
-        //TODO get_posts of Journal Posts
-        /*
-        *Get the blog Journal Entries
-        **/      
-   $args = array( 'post_type' => 'game', 'posts_per_page' => '10');
+   $args = array( 'post_type' => 'game', 'posts_per_page' => '10', 'meta_key' => 'game_coins', 'orderby' => 'meta_value_num');
    $game_posts = get_posts( $args ); // returns an array of posts
 
 ?>
-<?php foreach ( $game_posts as $post ) : setup_postdata( $post ); ?>
-           
+
+    <section class="top-ten">
+        <?php 
+        $i = 1;
+        foreach ( $game_posts as $post ) : setup_postdata( $post ); 
+
+        ?>
+            <div class="numbers">
+                <p><?php  echo $i++; ?></p>
+            </div>
+            
+           <div class="leader">
+               <div class="leader-info">
             <h3>
                 <a href="<?php echo get_the_permalink(); ?>">
                 <?php the_title(); ?> </a>
             </h3>
-            <?php the_post_thumbnail( 'thumbnail' ); ?>
             <div class="author">
 	            <h4> By: <?php echo CFS()->get ( 'game_author' ); ?> </h4>
             </div>
             <div class="coins">
 	            <h4> Coins: <?php echo CFS()->get ( 'game_coins' ); ?> </h4>
             </div>
-<?php endforeach; // End of the loop. ?>
         </div>
-		</main><!-- #main -->
-	</div><!-- #primary -->
+            <?php the_post_thumbnail( 'thumbnail' ); ?>
+            </div> <!-- end of leadernumber -->
+        <?php endforeach; // End of the game loop. ?>
+    </section>
+        </div>
+
+    </main><!-- #main -->
+</div><!-- #primary -->
 
 <?php get_footer(); ?>
