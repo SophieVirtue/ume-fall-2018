41264e43fe0fa40df47a438f53b521673d82f6de
Sophie Virtue <sophie.virtue@gmail.com>
2018-12-06 16:34:57 -0800

    Edits to the header

--- themes/ume/header.php
+++ themes/ume/header.php
@@ -18,43 +18,31 @@
 
 	<body <?php body_class(); ?>>
 		<div id="page" class="hfeed site footer-relative">
-
 			<a class="skip-link screen-reader-text" href="#content"><?php echo esc_html( 'Skip to content' ); ?></a>
-
+			
 			<header id="masthead" class="site-header" role="banner">
 				<div class="site-branding">
 					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				
 				</div><!-- .site-branding -->
-			<div class="main-header">
-			  <div class="left-column">
-			      <div class="logo">
-				  <img src="<?php echo get_template_directory_uri();?>/assets/images/lightning-right.png" alt="lightning-right"/>
-                  </div>
-                  <div class="label">
-				  <a href="<?php echo get_home_url(); ?>">
-
-				  <h3 class="site-title"> UME Arcade </h3>
-                   </a>
-                   </div>
-
-			
-              </div>
-			  <div class="right-column">
-			    <div class="search-form">
-                    <?php get_search_form(); ?>
-			    </div>
-			     <div class="hamburger-icon">
-			      
+				<div class="main-header">
+			  		<div class="left-column">
+			      		<div class="logo">
+				  			<img class="lightning" src="<?php echo get_template_directory_uri();?>/assets/images/lightning-right.png" alt="lightning-right"/>
+                  		</div>
+                  		<div class="label">
+				  			<a href="<?php echo get_home_url(); ?>">
+								<h3 class="site-title"> UME Arcade </h3>
+                   			</a>
+                   		</div>
+              		</div>
+			  		<div class="right-column">
+			    		<div class="search-form">
+                    		<?php get_search_form(); ?>
+			    		</div>
+			     		<div class="hamburger-icon"></div>
+			 		</div>
 				</div>
-			 </div>
-</div>
-
-
-
-				<nav id="site-navigation" class="main-navigation" role="navigation">
-					
-				</nav><!-- #site-navigation -->
+				<nav id="site-navigation" class="main-navigation" role="navigation"></nav>
 			</header><!-- #masthead -->
 
 			<div id="content" class="site-content">
