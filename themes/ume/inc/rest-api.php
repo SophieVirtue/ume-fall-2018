c9f0ff542634f9614d6f4003be0e60c204890013
Jenna Pegg <jenna.pegg.vic@gmail.com>
2018-12-10 14:47:57 -0800

    add custom endpoint for rest api

---
+++ themes/ume/inc/rest-api.php
@@ -0,0 +1,36 @@
+<?php 
+
+add_action( 'rest_api_init', 'ume_rest_route' );
+
+
+function ume_rest_route() {
+    register_rest_route( 
+        'ume/v1', 
+        '/coins/',
+        array(
+            'methods' => 'POST',
+            'callback' => 'ume_update_coins',
+        )
+    );
+}
+
+function ume_update_coins( $request ) {
+    $game_id = $request->get_param( 'id' ); 
+    $coins = CFS()->get( 'game_coins', $game_id );
+
+    if( $coins == "" ){
+        $coins = 1;
+    } else {
+        $coins += 1;
+    }
+
+    $field_data = array( 'game_coins' => $coins );
+    $post_data = array( 'ID' => $game_id, 'post_type' => 'game' );
+
+    CFS()->save( $field_data, $post_data );
+
+    return $coins;
+}
+
+
+?>
