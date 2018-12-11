c2b143447cf131c1148379f945d98b0a70f66395
Jenna Pegg <jenna.pegg.vic@gmail.com>
2018-12-11 13:35:31 -0800

    add popup to all play buttons on front page

--- themes/ume/template-parts/content-front.php
+++ themes/ume/template-parts/content-front.php
@@ -1,7 +1,7 @@
  <h3>  
- <?php the_title( printf( '<a href="%s" rel="bookmark">', esc_url( get_permalink() ) ) ); ?> </a>
+ <?php the_title( printf( '<a href="%s" rel="bookmark" class="play">', esc_url( get_permalink() ) ) ); ?> </a>
    </h3>
-            <?php the_post_thumbnail( 'thumbnail', printf( '<a href="%s" rel="bookmark">', esc_url( get_permalink() ) )); ?></a>
+            <?php the_post_thumbnail( 'thumbnail', printf( '<a href="%s" rel="bookmark" class="play">', esc_url( get_permalink() ) )); ?></a>
     <div class="author">
 	<h4> By: <?php echo CFS()->get ( 'game_author' ); ?> </h4>
     </div>
