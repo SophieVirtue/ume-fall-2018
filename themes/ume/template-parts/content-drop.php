ba12147ef0f3f87961ebdaa5ab3b0dc1f87a3923
Jenna Pegg <jenna.pegg.vic@gmail.com>
2018-12-12 12:09:02 -0800

    add grid style to dropdown

--- themes/ume/template-parts/content-drop.php
+++ themes/ume/template-parts/content-drop.php
@@ -9,27 +9,20 @@
    </h3>
    </div>
 
-    <div class="author">
-	<h4> By: <?php echo CFS()->get ( 'game_author' ); ?> </h4>
-    </div>
-    <div class="location">
-	<h4> <?php echo CFS()->get ( 'game_location' ); ?> </h4>
+    <div class="meta">
+	    <h4> By: <?php echo CFS()->get ( 'game_author' ); ?> </h4>
+	    <h4> <?php echo CFS()->get ( 'game_location' ); ?> </h4>
     </div>
     <div class="program">
-	<h4> <?php echo CFS()->get ( 'game_program' ); ?> </h4>
+	    <h4> <?php echo CFS()->get ( 'game_program' ); ?> </h4>
     </div>
-    <div class="design-button">
-    <?php $game_design = CFS()->get ( 'game_design' ); ?>
-    <?php if (is_array($game_design)) : ?>
-
-    <button>
-    <a href="<?php echo get_permalink($game_design[0]); ?>">
-        View Design Process
+    <div class="design">
+        <?php $game_design = CFS()->get ( 'game_design' ); ?>
+        <?php if (is_array($game_design)) : ?>
+        <a href="<?php echo get_permalink($game_design[0]); ?>">
+            View Design Process
         </a>
-</button>
-     
 <?php endif; ?>
-
 </div>
     <div class="coins">
 	<h4> Coins: <?php echo CFS()->get ( 'game_coins' ); ?> </h4>
@@ -53,16 +46,16 @@
     </div>
     
     <div class="thumbnail">
-    <a href="<?php echo esc_url( get_the_permalink()); ?>" class="play">
-        <?php the_post_thumbnail( 'thumbnail');?>
-    </a>
+        <a href="<?php echo esc_url( get_the_permalink()); ?>" class="play">
+            <?php the_post_thumbnail( 'thumbnail');?>
+        </a>
    </div>
     
-    <div class="gems">
+    <div class="gem">
         <h4>Prizes Earned <?php $gems =  ume_get_icons ( CFS()->get ( 'game_coins' ))?>
             <?php foreach($gems as $gem): 
                 echo $gem;
             endforeach; ?> </h4>
-</div>
+    </div>
 
 </article>
