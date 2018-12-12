e4782af789c8deddf34824c9529076e0495f70c0
Timmy <timgabrielnguyen@gmail.com>
2018-12-11 21:30:42 -0800

    Styling of Mobile for Design Process Page

--- themes/ume/template-parts/content-design.php
+++ themes/ume/template-parts/content-design.php
@@ -12,8 +12,53 @@
 		
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
-		
 	</header><!-- .entry-header -->
+
+
+       <div class="work-section">  
+	      	<div class="work-image">
+              <img class="work-flow-image" src="<?php echo CFS()->get ( 'work_flow_image' )?>">
+			  </div>
+          <div class="work-text">
+		         <?php echo CFS()->get ( 'work_flow_text' );?>
+			   </div>
+</div>
+
+   <div class="character-section">
+             <div class="character-image">
+			 <img class="character-image" src="<?php echo CFS()->get ( 'character_image' )?>">
+			 </div>
+			 <div class="character-text">
+	      	<?php echo CFS()->get ( 'character_text' );?>
+			  </div>
+</div>
+
+   <div class="obstacles-section">
+          <div class="obstacles-image">
+          <img class="obstacles-image" src="<?php echo CFS()->get ( 'obstacles_image' )?>">
+          </div>
+        <div class="obstacles-text">
+		<?php echo CFS()->get ( 'obstacles_text' );?>
+		</div>	
+   </div>
+
+       <div class="important-section">
+	     
+		 <div class="important-image">
+	    <img class="important-image" src="<?php echo CFS()->get ( 'important_image' )?>">
+		</div>
+
+         <div class="important-text">
+		<?php echo CFS()->get ( 'important_text' );?>
+		</div>
+
+        
+
+		</div>
+
+
+
+		
 
 	<!-- .entry-content -->
 </article><!-- #post-## -->
