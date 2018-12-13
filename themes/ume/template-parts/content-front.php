7ed5e20b0ccb5bd5ac555ec6c50ad6c5d5ac367e
Timmy <timgabrielnguyen@gmail.com>
2018-12-13 15:43:56 -0800

    Styling of Carousel Content on Mobile

--- themes/ume/template-parts/content-front.php
+++ themes/ume/template-parts/content-front.php
@@ -10,7 +10,7 @@
     <div class="author">
 	<h4> By: <?php echo CFS()->get ( 'game_author' ); ?> </h4>
     </div>
-    <button class="arrow">
-            <img  src="<?php echo get_template_directory_uri(); ?>/assets/images/arrow-pointing-down.jpg"/>
-    </button>
+    <div class="arrow">
+            <img  src="<?php echo get_template_directory_uri(); ?>/assets/images/down-arrow.png"/>
+</div>
     
