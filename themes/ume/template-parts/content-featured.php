ef5dec080608cbad01891b682c7fe230506c1bac
Jeff Liknes <jeff.liknes@gmail.com>
2018-12-13 15:48:56 -0800

    font added to featured games

--- themes/ume/template-parts/content-featured.php
+++ themes/ume/template-parts/content-featured.php
@@ -2,18 +2,18 @@
 
  <h3>  
 
-<a href="<?php echo esc_url( get_the_permalink()); ?>" class="play">
+<a href="<?php echo esc_url( get_the_permalink()); ?>" class="play-title-featured">
 <?php echo get_the_title(); ?>
 </a>
   </h3>
-  <a href="<?php echo esc_url( get_the_permalink()); ?>" class="play">
+  <a href="<?php echo esc_url( get_the_permalink()); ?>" class="play-thumbnail-featured">
        <?php the_post_thumbnail( 'thumbnail');?>
   </a>
    <div class="author">
    <h4> By: <?php echo CFS()->get ( 'game_author' ); ?> </h4>
    </div>
    <div class="play-button-featured">
-	<?php printf( '<a href="%s" class="play"><img src="'.get_template_directory_uri().'/assets/images/play-button.png"/></a>', esc_url( get_permalink() ) ); ?>
+	<?php printf( '<a href="%s" class="play-button-img"><img src="'.get_template_directory_uri().'/assets/images/play-button.png"/></a>', esc_url( get_permalink() ) ); ?>
 	
 	</div>
 
