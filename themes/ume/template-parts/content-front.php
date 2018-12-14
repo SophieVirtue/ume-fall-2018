d3011e6bc77751ed41e1051563c5b3cdfc0a8ead
Jeff Liknes <jeff.liknes@gmail.com>
2018-12-14 09:46:00 -0800

    merge conflict

--- themes/ume/template-parts/content-front.php
+++ themes/ume/template-parts/content-front.php
@@ -13,8 +13,5 @@
     <div class="arrow">
             <img  src="<?php echo get_template_directory_uri(); ?>/assets/images/down-arrow.png"/>
 </div>
-<<<<<<< HEAD
     
     
-=======
->>>>>>> d74c86da749063f2e72697904a9974127fd824e2
