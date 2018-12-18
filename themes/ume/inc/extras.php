3e0e841c143d3dbcc33523e47507a3dcd4a559bd
Jenna Pegg <jenna.pegg.vic@gmail.com>
2018-12-18 11:18:31 -0800

    add alts to gems

--- themes/ume/inc/extras.php
+++ themes/ume/inc/extras.php
@@ -77,41 +77,41 @@ function ume_get_icons ($game_coins) {
 	$gems = array();
 
 	if ($game_coins >= 5) {
-	$gems[0]= '<img class="gem" src="'.get_template_directory_uri().'/assets/images/gems/diamond.png">';
+	$gems[0]= '<img class="gem" src="'.get_template_directory_uri().'/assets/images/gems/diamond.png" alt="diamond">';
 	}
 
  if ($game_coins >= 10) {
-	$gems[1]= '<img class="gem" src="'.get_template_directory_uri().'/assets/images/gems/blue-diamond.png">';
+	$gems[1]= '<img class="gem" src="'.get_template_directory_uri().'/assets/images/gems/blue-diamond.png">alt="blue diamond"';
 }
  if ($game_coins >= 20) {
-	$gems[2]= '<img class="gem" src="'.get_template_directory_uri().'/assets/images/gems/pink.png">';
+	$gems[2]= '<img class="gem" src="'.get_template_directory_uri().'/assets/images/gems/pink.png" alt="pink gem">';
 }
  if ($game_coins >= 30) {
-    $gems[3]= '<img class="gem" src="'.get_template_directory_uri().'/assets/images/gems/mystery-box-gems/gold-star.png">';
+    $gems[3]= '<img class="gem" src="'.get_template_directory_uri().'/assets/images/gems/mystery-box-gems/gold-star.png" alt="gold-star">';
 }
  if ($game_coins >= 50) {
-	$gems[4]= '<img class="gem" src="'.get_template_directory_uri().'/assets/images/gems/purple.png">';
+	$gems[4]= '<img class="gem" src="'.get_template_directory_uri().'/assets/images/gems/purple.png" alt="purple gem">';
 }
  if ($game_coins >= 75) {
-	$gems[5]= '<img class="gem" src="'.get_template_directory_uri().'/assets/images/gems/orange.png">';
+	$gems[5]= '<img class="gem" src="'.get_template_directory_uri().'/assets/images/gems/orange.png" alt="orange gem">';
 }
  if ($game_coins >= 100) {
-    $gems[6]= '<img class="gem" src="'.get_template_directory_uri().'/assets/images/gems/light-blue.png">';
+    $gems[6]= '<img class="gem" src="'.get_template_directory_uri().'/assets/images/gems/light-blue.png" alt="light blue gem">';
 }
  if ($game_coins >= 150) {
-	$gems[7]= '<img class="gem" src="'.get_template_directory_uri().'/assets/images/gems/mystery-box-gems/heart.png">';
+	$gems[7]= '<img class="gem" src="'.get_template_directory_uri().'/assets/images/gems/mystery-box-gems/heart.png" alt="heart gem">';
 }
  if ($game_coins >= 200) {
-	$gems[8]= '<img class="gem" src="'.get_template_directory_uri().'/assets/images/gems/red.png">';
+	$gems[8]= '<img class="gem" src="'.get_template_directory_uri().'/assets/images/gems/red.png" alt="red gem">';
 }
  if ($game_coins >= 250) {
-	$gems[9]= '<img class="gem" src="'.get_template_directory_uri().'/assets/images/gems/yellow.png">';
+	$gems[9]= '<img class="gem" src="'.get_template_directory_uri().'/assets/images/gems/yellow.png" alt="yellow gem">';
 }
 if ($game_coins >= 300) {
-	$gems[10]= '<img class="gem" src="'.get_template_directory_uri().'/assets/images/gems/green-jewel.png">';
+	$gems[10]= '<img class="gem" src="'.get_template_directory_uri().'/assets/images/gems/green-jewel.png" alt="green gem">';
 }
  if ($game_coins >= 500) {
-    $gems[11]= '<img class="gem" src="'.get_template_directory_uri().'/assets/images/gems/mystery-box-gems/raindrop.png">';
+    $gems[11]= '<img class="gem" src="'.get_template_directory_uri().'/assets/images/gems/mystery-box-gems/raindrop.png" alt="raindrop gem">';
 }
 
 if(count($gems) > 3) {
