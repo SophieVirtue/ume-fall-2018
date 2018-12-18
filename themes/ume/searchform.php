6921a1c1012842d5acc4d819fa5e6ad1c638a5fb
Jenna Pegg <jenna.pegg.vic@gmail.com>
2018-12-18 10:32:58 -0800

    clean code and fix spacing

--- themes/ume/searchform.php
+++ themes/ume/searchform.php
@@ -1,19 +1,20 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
 	<fieldset>
 		<div class="search-form">
-		<div class="search-submit">
-			<span class="icon-search" aria-hidden="true">
-				<i class="fa fa-search"></i>
-			</span>
-			<span class="screen-reader-text"><?php echo esc_html( 'Search' ); ?></span>
-</div>
-         <div class="submit-text">
-		<label>
-			<input type="search" class="search-field" placeholder="Search" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="Search for:" required/>
-		</label>
-</div>
-</div>
-		<!--<input id="search-submit" type="submit" class="search">-->
+			<div class="search-submit">
+				<span class="icon-search" aria-hidden="true">
+					<i class="fa fa-search"></i>
+				</span>
+				<span class="screen-reader-text">
+					<?php echo esc_html( 'Search' ); ?>
+				</span>
+			</div><!-- end of search-submit -->
+         	<div class="submit-text">
+				<label>
+					<input type="search" class="search-field" placeholder="Search" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="Search for:" required/>
+				</label>
+			</div><!-- end of submit-text -->
+		</div><!-- end of search-form -->
 	</fieldset>
 </form>
 
