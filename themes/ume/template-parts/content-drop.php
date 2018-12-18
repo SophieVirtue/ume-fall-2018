affe3d206d59065fe00a6c02f59e327d056f1c95
Jenna Pegg <jenna.pegg.vic@gmail.com>
2018-12-18 11:05:39 -0800

    clean up code and fix spacing

--- themes/ume/template-parts/content-drop.php
+++ themes/ume/template-parts/content-drop.php
@@ -5,14 +5,22 @@
                 <?php echo get_the_title(); ?>
             </a>
         </h3>
-    </div>
+    </div><!-- end of title -->
     <div class="meta">
-	    <h4> By: <?php echo CFS()->get ( 'game_author' ); ?> </h4>
-	    <h4> <?php echo CFS()->get ( 'game_location' ); ?> </h4>
-    </div>
+	    <h4> 
+            By: <?php echo CFS()->get ( 'game_author' ); ?> 
+        </h4>
+	    <h4> 
+            <?php echo CFS()->get ( 'game_location' ); ?> 
+        </h4>
+    </div><!-- end of meta -->
     <div class="program">
-	    <h4><a href="https://ume.academy/programs/"><?php echo CFS()->get ( 'game_program' ); ?> Program</a></h4>
-    </div>
+	    <h4>
+            <a href="https://ume.academy/programs/"><?php echo CFS()->get ( 'game_program' ); ?> 
+                Program
+            </a>
+        </h4>
+    </div><!-- end of program -->
     <div class="view-design">
         <?php $game_design = CFS()->get ( 'game_design' ); ?>
         <?php if (is_array($game_design)) : ?>
@@ -20,13 +28,16 @@
             View Design Process
         </a>
         <?php endif; ?>
-    </div>
+    </div><!-- end of view-design -->
     <div class="coins">
-	    <h4> <img src="<?php echo get_template_directory_uri(); ?>/assets/images/coin.png" alt=""> <?php echo CFS()->get ( 'game_coins' ); ?> </h4>
-    </div>
+	    <h4> 
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/coin.png" alt=""> <?php echo CFS()->get ( 'game_coins' ); ?> 
+        </h4>
+    </div><!-- end of coins -->
     <div class="buttons">
         <p class="download">
-            <a href="./download?id=<?php the_ID(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/download-icon.png" alt="Download"/></a>
+            <a href="./download?id=<?php the_ID(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/download-icon.png" alt="Download"/>
+            </a>
         </p>
         <p class="play-game">
             <a href="<?php echo esc_url( get_the_permalink()); ?>" class="play">
@@ -39,18 +50,21 @@
             </a>
             <input type="text" class="hidden-copy" value="<?php echo get_the_permalink() ?>">
         </p> 
-    </div>
+    </div><!-- end of buttons -->
     <div class="thumbnail">
         <a href="<?php echo esc_url( get_the_permalink()); ?>" class="play">
             <?php the_post_thumbnail( 'thumbnail');?>
         </a>
-    </div>
+    </div><!-- end of thumbnail -->
     <div class="gem">
-        <h4>Prizes Earned: </h4>
-        <div><?php $gems =  ume_get_icons ( CFS()->get ( 'game_coins' ))?>
+        <h4>
+            Prizes Earned: 
+        </h4>
+        <div>
+            <?php $gems =  ume_get_icons ( CFS()->get ( 'game_coins' ))?>
             <?php foreach($gems as $gem): 
                 echo $gem;
             endforeach; ?> 
-        </div>
-    </div>
+        </div><!-- end of foreach div -->
+    </div><!-- end of gem -->
 </article>
--- themes/ume/template-parts/content-front.php
+++ themes/ume/template-parts/content-front.php
@@ -1,17 +1,14 @@
- <h3>  
-
- <a href="<?php echo esc_url( get_the_permalink()); ?>" class="play">
- <?php echo get_the_title(); ?>
- </a>
-   </h3>
-   <a href="<?php echo esc_url( get_the_permalink()); ?>" class="play">
-        <?php the_post_thumbnail( 'thumbnail');?>
-   </a>
-    <div class="author">
+<h3>
+    <a href="<?php echo esc_url( get_the_permalink()); ?>" class="play">
+        <?php echo get_the_title(); ?>
+    </a>
+</h3>
+<a href="<?php echo esc_url( get_the_permalink()); ?>" class="play">
+    <?php the_post_thumbnail( 'thumbnail');?>
+</a>
+<div class="author">
 	<h4> By: <?php echo CFS()->get ( 'game_author' ); ?> </h4>
-    </div>
-    <div class="arrow">
-            <img  src="<?php echo get_template_directory_uri(); ?>/assets/images/down-arrow.png" alt="Dropdown Arrow"/>
-</div>
-    
-    
+</div><!-- end of author -->
+<div class="arrow">
+    <img  src="<?php echo get_template_directory_uri(); ?>/assets/images/down-arrow.png" alt="Dropdown Arrow"/>
+</div><!-- end of arrow -->
